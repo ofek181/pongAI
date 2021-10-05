0c33c04f0103c7f1b941ab25840fbebbac9072ab
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-05 12:00:01 +0300

    added docstring

--- pongGame/position_module.py
+++ pongGame/position_module.py
@@ -2,10 +2,33 @@ from abc import ABC, abstractmethod
 
 
 class Position(metaclass=ABC):
+    """
+        A class for implementing x,y position on the screen
+        ----------------------------
+        Attributes
+        ----------------------------
+        x_pos
+            x position on the screen
+        y_pos
+            y position on the screen
+        ----------------------------
+        Methods
+        ----------------------------
+        __init__(self):
+            Constructs all the necessary attributes for the Position object.
+        set_pos(self):
+            abstract method for setting the attributes
+    """
     def __init__(self):
+        """
+            Constructs all the necessary attributes for the Position object.
+        """
         self.x_pos = 0
         self.y_pos = 0
 
     @abstractmethod
     def set_pos(self):
+        """
+            Abstract method for setting position to later be implemented.
+        """
         pass
