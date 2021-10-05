349e4f4b74a9890e8b774adb36c6f9c07497f1ff
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-05 11:59:43 +0300

    added docstring

--- pongGame/velocity_module.py
+++ pongGame/velocity_module.py
@@ -2,10 +2,34 @@ from abc import ABC, abstractmethod
 
 
 class Velocity(metaclass=ABC):
+    """
+        A class for implementing x,y velocity on the screen.
+        ----------------------------
+        Attributes
+        ----------------------------
+        x_vel
+            x velocity on the screen.
+        y_vel
+            y velocity on the screen.
+        ----------------------------
+        Methods
+        ----------------------------
+        __init__(self):
+            Constructs all the necessary attributes for the Velocity object.
+        set_vel(self):
+            abstract method for setting the velocity.
+    """
+
     def __init__(self):
+        """
+            Constructs all the necessary attributes for the Velocity object.
+        """
         self.x_vel = 0
         self.y_vel = 0
 
     @abstractmethod
     def set_vel(self):
+        """
+            Abstract method for setting velocity to later be implemented.
+        """
         pass
