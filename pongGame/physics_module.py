d75ce092ca5110eeadf2ffbde8992f2ac224859b
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-05 20:28:09 +0300

    implemented the physics methods

--- pongGame/physics_module.py
+++ pongGame/physics_module.py
@@ -1,7 +1,8 @@
-from .position_module import Position
+import math
 from .velocity_module import Velocity
 from .ball_module import Ball
 from .paddle_module import Paddle
+from consts_file import DisplayConsts
 
 
 class Physics:
@@ -12,64 +13,59 @@ class Physics:
         ----------------------------
         calc_ball_velocity(ball: Ball, paddle: Paddle) -> Velocity:
          Calculates the velocity of the ball for the following frame.
-        calc_next_ball_position(ball: Ball, paddle: Paddle) -> Position:
-         Calculates the position of the ball for the following frame.
-        calc_next_paddle_position(paddle: Paddle) -> Position:
-         Calculates the position of the paddle for the following frame.
-        calc_ball_paddle_intersection(ball: Ball, paddle: Paddle) -> Position:
+        ball_screen_intersection(ball: Ball) -> bool:
+         Checks whether an intersection of the ball with the screen occurred.
+        ball_paddle_intersection(ball: Ball, paddle: Paddle) -> [bool, int]:
          Calculates the intersection of the ball with the paddle.
     """
 
-    @staticmethod
-    def calc_ball_velocity(ball: Ball, paddle: Paddle) -> Velocity:
+    def calc_ball_velocity(self, ball: Ball, paddle: Paddle) -> Velocity:
         """
             Calculates the velocity of the ball for the following frame.
         """
-        pass
+        vel = Velocity(ball.x_vel, ball.y_vel)
+
+        if self.ball_paddle_intersection(ball, paddle)[0]:
+            bounce_angle = self.ball_paddle_intersection(ball, paddle)[1] * 90
+            ball_speed = math.sqrt(ball.x_vel**2 + ball.y_vel**2)
+            new_x_vel = -ball_speed * math.cos(bounce_angle)
+            new_y_vel = ball_speed * math.sin(bounce_angle)
+            vel = Velocity(new_x_vel, new_y_vel)
+
+        if self.ball_screen_intersection(ball):
+            new_y_vel = -ball.y_vel
+            vel = Velocity(ball.x_vel, new_y_vel)
+
+        return vel
 
     @staticmethod
-    def calc_next_ball_position(ball: Ball, paddle: Paddle) -> Position:
-        """
-            Calculates the position of the ball for the following frame.
-        """
-        pass
+    def ball_screen_intersection(ball: Ball) -> bool:
+        if ball.y_pos - ball.m_radius == 0 or ball.y_pos + ball.m_radius == DisplayConsts.SCREEN_HEIGHT:
+            return True
+        return False
 
     @staticmethod
-    def calc_next_paddle_position(paddle: Paddle) -> Position:
-        """
-            Calculates the position of the paddle for the following frame.
-        """
-        pass
-
-    @staticmethod
-    def ball_paddle_intersection(ball: Ball, paddle: Paddle) -> [bool, int, str]:
+    def ball_paddle_intersection(ball: Ball, paddle: Paddle) -> [bool, float]:
         """
             Calculates the intersection of the ball with the paddle.
             Returns:
                 collision: whether or not a collision occurred.
                 dist: collision distance between the ball and paddle on the y_axis.
-                section: section of the paddle in which the collision occurred.
         """
         collision = False
-        dist = 0
-        section = "None"
+        normalized_dist = 0
 
         if ball.x_pos + ball.m_radius == paddle.x_pos - paddle.m_size[0]:  # right paddle intersection
             collision = True
         if ball.x_pos - ball.m_radius == paddle.x_pos + paddle.m_size[0]:  # left paddle intersection
             collision = True
         if collision:
-            ball_center = [ball.x_pos, ball.y_pos]
-            paddle_center = [paddle.x_pos + paddle.m_size[0] / 2,
-                             paddle.y_pos + paddle.m_size[1] / 2]
-            if ball_center[1] <= paddle_center[1]:  # ball is on the top side of the paddle
-                dist = paddle_center[1] - ball_center[1]
-                section = "TOP"
+            # dist is positive if the ball is on the top of the paddle
+            normalized_dist = (ball.y_pos - (paddle.y_pos + paddle.m_size[1] / 2)) / paddle.m_size[1]
 
-            else:  # ball is on the bottom side of the paddle
-                dist = ball_center[1] - paddle_center[1]
-                section = "BOT"
+            if abs(normalized_dist) > 1/2:
+                raise Exception("Distance from paddle center during intersection is not legal")
 
-        return collision, dist, section
+        return collision, normalized_dist
 
 
