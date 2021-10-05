1dd643be8bf39a1c4f875a22f300c3279b645f27
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-05 18:11:57 +0300

    implemented the intersection method

--- pongGame/physics_module.py
+++ pongGame/physics_module.py
@@ -42,9 +42,34 @@ class Physics:
         pass
 
     @staticmethod
-    def calc_ball_paddle_intersection(ball: Ball, paddle: Paddle) -> Position:
+    def ball_paddle_intersection(ball: Ball, paddle: Paddle) -> [bool, int, str]:
         """
             Calculates the intersection of the ball with the paddle.
+            Returns:
+                collision: whether or not a collision occurred.
+                dist: collision distance between the ball and paddle on the y_axis.
+                section: section of the paddle in which the collision occurred.
         """
-        pass
+        collision = False
+        dist = 0
+        section = "None"
+
+        if ball.x_pos + ball.m_radius == paddle.x_pos - paddle.m_size[0]:  # right paddle intersection
+            collision = True
+        if ball.x_pos - ball.m_radius == paddle.x_pos + paddle.m_size[0]:  # left paddle intersection
+            collision = True
+        if collision:
+            ball_center = [ball.x_pos, ball.y_pos]
+            paddle_center = [paddle.x_pos + paddle.m_size[0] / 2,
+                             paddle.y_pos + paddle.m_size[1] / 2]
+            if ball_center[1] <= paddle_center[1]:  # ball is on the top side of the paddle
+                dist = paddle_center[1] - ball_center[1]
+                section = "TOP"
+
+            else:  # ball is on the bottom side of the paddle
+                dist = ball_center[1] - paddle_center[1]
+                section = "BOT"
+
+        return collision, dist, section
+
 
