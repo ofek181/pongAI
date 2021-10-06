9a3135d5efe12ed7febf1b8aa885ef5255da7d0b
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-07 00:56:29 +0300

    bugfixes on physics of the game

--- pongGame/physics_module.py
+++ pongGame/physics_module.py
@@ -3,7 +3,7 @@ from .velocity_module import Velocity
 from .ball_module import Ball
 from .paddle_module import Paddle
 from .audio_module import Audio
-from .consts_file import DisplayConsts
+from .consts_file import DisplayConsts, BallConsts
 
 
 class Physics:
@@ -16,25 +16,43 @@ class Physics:
          Calculates the velocity of the ball for the following frame.
         ball_screen_intersection(ball: Ball) -> bool:
          Checks whether an intersection of the ball with the screen occurred.
-        ball_paddle_intersection(ball: Ball, paddle: Paddle) -> [bool, int]:
+        ball_paddle_intersection(ball: Ball, paddle: Paddle) -> [bool, bool, int]:
          Calculates the intersection of the ball with the paddle.
+        is_score(ball: Ball) -> [bool, bool]
+         returns a tuple of bool depending on which paddle scored
     """
 
     @staticmethod
-    def calc_ball_velocity(ball: Ball, paddle: Paddle) -> Velocity:
+    def calc_ball_velocity(ball: Ball, paddle_left: Paddle, paddle_right: Paddle) -> Velocity:
         """
             Calculates the velocity of the ball for the following frame.
         """
+        acceleration = 0.2
         vel = Velocity(ball.x_vel, ball.y_vel)
 
-        if Physics.__ball_paddle_intersection(ball, paddle)[0]:
-            bounce_angle = Physics.__ball_paddle_intersection(ball, paddle)[1] * 90
-            ball_speed = math.sqrt(ball.x_vel**2 + ball.y_vel**2)
-            new_x_vel = -ball_speed * math.cos(bounce_angle)
-            new_y_vel = ball_speed * math.sin(bounce_angle)
-            vel = Velocity(new_x_vel + 1, new_y_vel + 1)  # make ball accelerate each intersection
+        # intersection with the left paddle
+        if Physics.__ball_paddle_intersection(ball, paddle_left, paddle_right)[0]:
+            angle = Physics.__ball_paddle_intersection(ball, paddle_left, paddle_right)[2] * 90
+            bounce = math.copysign(abs(min([angle, BallConsts.MAX_BOUNCE_ANGLE], key=abs)), angle)
+            ball_speed = math.sqrt(ball.x_vel**2 + ball.y_vel**2) + acceleration  # make the ball accelerate
+            x_vel = ball_speed * math.cos(math.degrees(bounce))
+            new_x_vel = max([x_vel, BallConsts.MIN_X_VEL], key=abs)
+            new_y_vel = ball_speed * math.sin(math.degrees(bounce))
+            vel = Velocity(new_x_vel, new_y_vel)  # make ball accelerate each intersection
             Audio.sound_hit()
 
+        # intersection with the right paddle
+        if Physics.__ball_paddle_intersection(ball, paddle_left, paddle_right)[1]:
+            angle = Physics.__ball_paddle_intersection(ball, paddle_left, paddle_right)[2] * 90
+            bounce = math.copysign(abs(min([angle, BallConsts.MAX_BOUNCE_ANGLE], key=abs)), angle)
+            ball_speed = math.sqrt(ball.x_vel ** 2 + ball.y_vel ** 2) + acceleration
+            x_vel = -ball_speed * math.cos(math.degrees(bounce))
+            new_x_vel = max([x_vel, -BallConsts.MIN_X_VEL], key=abs)
+            new_y_vel = ball_speed * math.sin(math.degrees(bounce))
+            vel = Velocity(new_x_vel, new_y_vel)  # make ball accelerate each intersection
+            Audio.sound_hit()
+
+        # intersection with the screen
         if Physics.__ball_screen_intersection(ball):
             new_y_vel = -ball.y_vel
             vel = Velocity(ball.x_vel, new_y_vel)
@@ -48,28 +66,56 @@ class Physics:
         return False
 
     @staticmethod
-    def __ball_paddle_intersection(ball: Ball, paddle: Paddle) -> [bool, float]:
+    def __ball_paddle_intersection(ball: Ball, paddle_left: Paddle, paddle_right: Paddle) -> [bool, bool, float]:
         """
             Calculates the intersection of the ball with the paddle.
             Returns:
                 collision: whether or not a collision occurred.
                 dist: collision distance between the ball and paddle on the y_axis.
         """
-        collision = False
+        collision_right = False
+        collision_left = False
         normalized_dist = 0
+        buffer = 5
 
-        if ball.x_pos + ball.m_radius == paddle.x_pos and ball.y_pos == paddle.y_pos:  # right paddle intersection
-            collision = True
-        if ball.x_pos - ball.m_radius == paddle.x_pos + paddle.m_size[0] and ball.y_pos == paddle.y_pos:  # left paddle intersection
-            collision = True
-        if collision:
-            # dist is positive if the ball is on the bottom of the paddle
-            normalized_dist = (ball.y_pos - (paddle.y_pos + paddle.m_size[1] / 2)) / paddle.m_size[1]
+        # right paddle intersection
+        if paddle_right.x_pos+paddle_right.m_size[0]+buffer >= ball.x_pos+ball.m_radius >= paddle_right.x_pos-buffer:
+            if ball.x_vel > 0:
+                if paddle_right.y_pos <= ball.y_pos <= paddle_right.y_pos + paddle_right.m_size[1]:
+                    collision_right = True
+                    normalized_dist = (paddle_right.y_pos + paddle_right.m_size[1] / 2
+                                       - ball.y_pos) / paddle_right.m_size[1]
+
+        # left paddle intersection
+        if paddle_left.x_pos-buffer <= ball.x_pos - ball.m_radius <= paddle_left.x_pos+paddle_left.m_size[0]+buffer:
+            if ball.x_vel < 0:
+                if paddle_left.y_pos <= ball.y_pos <= paddle_left.y_pos + paddle_left.m_size[1]:
+                    collision_left = True
+                    normalized_dist = (paddle_left.y_pos + paddle_left.m_size[1] / 2
+                                       - ball.y_pos) / paddle_left.m_size[1]
+
+        if abs(normalized_dist) > 1/2:
+            raise Exception("Distance during intersection Error")
+
+        if collision_right or collision_left:
             print(normalized_dist)
 
-            if abs(normalized_dist) > 1/2:
-                raise Exception("Distance during intersection Error")
+        return collision_left, collision_right, normalized_dist
 
-        return collision, normalized_dist
+    @staticmethod
+    def is_score(ball: Ball) -> [bool, bool]:
+        """
+           Checks if a goal occurred
+           Returns:
+               return a tuple of bool depending on who scored.
+       """
+        if ball.x_pos >= DisplayConsts.SCREEN_WIDTH:
+            Audio.sound_score()
+            return [True, False]
+        if ball.x_pos <= 0:
+            Audio.sound_score()
+            return [False, True]
+
+        return [False, False]
 
 
