80715e16a8c5b1ae1bf765f7c0e087f1e69fbf49
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-06 18:22:58 +0300

    changes

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-from pongGame.display_module import Display
-from pongGame.ball_module import Ball
-from pongGame.paddle_module import Paddle
+from pongGame.pong_module import Pong
 
 
 def main():
-    ball = Ball()
-    paddle_bot = Paddle()
-    paddle_top = Paddle(x_pos=50, y_pos=200)
-    display = Display()
-    while True:
-        display.animate_game(paddle_bot, paddle_top, ball)
-        display.show_score(2, 4)
-        display.show_winner("player 1 wins!")
+    game = Pong()
+    game.run_game()
 
 
 if __name__ == '__main__':
