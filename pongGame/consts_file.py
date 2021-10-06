06d493198ca41a866d5a4a1017c78cc6556bce41
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-07 00:54:20 +0300

    changed consts for the game

--- pongGame/consts_file.py
+++ pongGame/consts_file.py
@@ -4,9 +4,9 @@ local_dir_audio = os.path.join(os.path.dirname(__file__), "audio")
 
 
 class DisplayConsts:
-    FPS = 30
-    SCREEN_WIDTH = 950
-    SCREEN_HEIGHT = 550
+    FPS = 144
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 400
     FONT_COLOR = (255, 255, 255)
     LINE_FONT = (50, 50, 50)
     LINE_WIDTH = 10
@@ -24,12 +24,15 @@ class BallConsts:
     BALL_RADIUS = 10
     BALL_STARTING_POSITION_X = DisplayConsts.SCREEN_WIDTH // 2
     BALL_STARTING_POSITION_Y = DisplayConsts.SCREEN_HEIGHT // 2
-    BALL_STARTING_VELOCITY_X = 10
+    BALL_STARTING_VELOCITY_X = 3
     BALL_STARTING_VELOCITY_Y = 0
+    MIN_X_VEL = BALL_STARTING_VELOCITY_X
+    MAX_BOUNCE_ANGLE = 50
 
 
 class PaddleConsts:
     PADDLE_SIZE = [10, 60]
+    PADDLE_VELOCITY = 5
 
 
 class GameConsts:
