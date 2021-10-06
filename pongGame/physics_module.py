11ae8681b76056b73c078e9f921ea3635f6a588d
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-06 18:22:06 +0300

    needs work

--- pongGame/physics_module.py
+++ pongGame/physics_module.py
@@ -2,7 +2,8 @@ import math
 from .velocity_module import Velocity
 from .ball_module import Ball
 from .paddle_module import Paddle
-from consts_file import DisplayConsts
+from .audio_module import Audio
+from .consts_file import DisplayConsts
 
 
 class Physics:
@@ -19,33 +20,35 @@ class Physics:
          Calculates the intersection of the ball with the paddle.
     """
 
-    def calc_ball_velocity(self, ball: Ball, paddle: Paddle) -> Velocity:
+    @staticmethod
+    def calc_ball_velocity(ball: Ball, paddle: Paddle) -> Velocity:
         """
             Calculates the velocity of the ball for the following frame.
         """
         vel = Velocity(ball.x_vel, ball.y_vel)
 
-        if self.ball_paddle_intersection(ball, paddle)[0]:
-            bounce_angle = self.ball_paddle_intersection(ball, paddle)[1] * 90
+        if Physics.__ball_paddle_intersection(ball, paddle)[0]:
+            bounce_angle = Physics.__ball_paddle_intersection(ball, paddle)[1] * 90
             ball_speed = math.sqrt(ball.x_vel**2 + ball.y_vel**2)
             new_x_vel = -ball_speed * math.cos(bounce_angle)
             new_y_vel = ball_speed * math.sin(bounce_angle)
             vel = Velocity(new_x_vel + 1, new_y_vel + 1)  # make ball accelerate each intersection
+            Audio.sound_hit()
 
-        if self.ball_screen_intersection(ball):
+        if Physics.__ball_screen_intersection(ball):
             new_y_vel = -ball.y_vel
             vel = Velocity(ball.x_vel, new_y_vel)
 
         return vel
 
     @staticmethod
-    def ball_screen_intersection(ball: Ball) -> bool:
-        if ball.y_pos - ball.m_radius == 0 or ball.y_pos + ball.m_radius == DisplayConsts.SCREEN_HEIGHT:
+    def __ball_screen_intersection(ball: Ball) -> bool:
+        if ball.y_pos - ball.m_radius <= 0 or ball.y_pos + ball.m_radius >= DisplayConsts.SCREEN_HEIGHT:
             return True
         return False
 
     @staticmethod
-    def ball_paddle_intersection(ball: Ball, paddle: Paddle) -> [bool, float]:
+    def __ball_paddle_intersection(ball: Ball, paddle: Paddle) -> [bool, float]:
         """
             Calculates the intersection of the ball with the paddle.
             Returns:
@@ -55,13 +58,14 @@ class Physics:
         collision = False
         normalized_dist = 0
 
-        if ball.x_pos + ball.m_radius == paddle.x_pos - paddle.m_size[0]:  # right paddle intersection
+        if ball.x_pos + ball.m_radius == paddle.x_pos and ball.y_pos == paddle.y_pos:  # right paddle intersection
             collision = True
-        if ball.x_pos - ball.m_radius == paddle.x_pos + paddle.m_size[0]:  # left paddle intersection
+        if ball.x_pos - ball.m_radius == paddle.x_pos + paddle.m_size[0] and ball.y_pos == paddle.y_pos:  # left paddle intersection
             collision = True
         if collision:
             # dist is positive if the ball is on the bottom of the paddle
             normalized_dist = (ball.y_pos - (paddle.y_pos + paddle.m_size[1] / 2)) / paddle.m_size[1]
+            print(normalized_dist)
 
             if abs(normalized_dist) > 1/2:
                 raise Exception("Distance during intersection Error")
