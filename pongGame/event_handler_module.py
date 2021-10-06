76bf424fe5e4359f47fd30de693afe8c6831d175
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-06 16:17:49 +0300

    implemented both parties event handling

--- pongGame/event_handler_module.py
+++ pongGame/event_handler_module.py
@@ -8,12 +8,14 @@ class EventHandler:
         A static class to implement the event handler of the game.
         Methods
         ----------------------------
-        handle_events() -> int:
-         a function to handle the user events during the game run time.
+        handle_left_events() -> int:
+         a function to handle the left user events during the game run time.
+        handle_right_events() -> int:
+         a function to handle the right user events during the game run time.
     """
 
     @staticmethod
-    def handle_events() -> int:
+    def handle_left_events() -> int:
         """
         Handles the user's inputs during the game.
         Returns
@@ -34,6 +36,27 @@ class EventHandler:
             return Action.LEFT_PADDLE_MOVE_UP.value
         if keys[pygame.K_s]:
             return Action.LEFT_PADDLE_MOVE_DOWN.value
+
+        return Action.NO_ACTION.value
+
+    @staticmethod
+    def handle_right_events() -> int:
+        """
+        Handles the user's inputs during the game.
+        Returns
+            The action ID according to its enum class
+
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return Action.QUIT.value
+                    # pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+        keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             return Action.RIGHT_PADDLE_MOVE_UP.value
         if keys[pygame.K_DOWN]:
