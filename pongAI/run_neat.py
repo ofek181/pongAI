25057460b08ad3789b3d721379dad6ce624d1e80
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-11 00:07:30 +0300

    implemented train and test helper functions

--- pongAI/run_neat.py
+++ pongAI/run_neat.py
@@ -2,7 +2,7 @@ import os
 from .neat_algorithm import NeatAI
 
 
-def start_neat():
+def train():
     """
         Determine path to configuration file. This path manipulation is
         here so that the script will run successfully regardless of the
@@ -12,5 +12,15 @@ def start_neat():
     config_path = os.path.join(local_dir, 'config-feedforward.txt')
     NeatAI.train(config_path)  # train
 
-    # NeatAI.test(config_path)  # test
+
+def test():
+    """
+        Determine path to configuration file. This path manipulation is
+        here so that the script will run successfully regardless of the
+        current working directory.
+    """
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    NeatAI.test(config_path)  # test
+
 
