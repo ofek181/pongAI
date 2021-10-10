70a2220a8aa40b059cca811c879f4a74e684f2dd
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-11 00:07:58 +0300

    main for running the project

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import pygame_menu
 from pongGame.pong_module import Pong
-from pongAI.run_neat import start_neat
+from pongAI.run_neat import test, train
 from pongGame.consts_file import DisplayConsts
 
 
@@ -8,11 +8,11 @@ def main():
     pong = Pong()
     menu = pygame_menu.Menu('Pong', DisplayConsts.SCREEN_WIDTH, DisplayConsts.SCREEN_HEIGHT,
                             theme=pygame_menu.themes.THEME_DARK)
-    menu.add.button('Play against a neural network', pong.run_game)
+    menu.add.button('Play against a neural network', test)
     menu.add.button('Play against a human', pong.run_game)
     menu.add.button('Exit', pygame_menu.events.EXIT)
     menu.mainloop(pong.display.screen)
 
 
 if __name__ == '__main__':
-    start_neat()
+    main()
