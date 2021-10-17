95cc959db8c94cc7647699a9e6ee7dcf4c5e99d8
Ofek Ophir <ophir.ofek@gmail.com>
2021-10-17 18:56:32 +0300

    added the option to train a network from the main menu

--- start.py
+++ start.py
@@ -8,7 +8,8 @@ def main():
     pong = Pong()
     menu = pygame_menu.Menu('Pong', DisplayConsts.SCREEN_WIDTH, DisplayConsts.SCREEN_HEIGHT,
                             theme=pygame_menu.themes.THEME_DARK)
-    menu.add.button('Play against a neural network', AI.test)
+    menu.add.button('Play against the fittest neural network', AI.test)
+    menu.add.button('Train a neural network with NEAT', AI.train)
     menu.add.button('Play against a human', pong.run_game)
     menu.add.button('Exit', pygame_menu.events.EXIT)
     menu.mainloop(pong.display.screen)
